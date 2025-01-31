04c3d53f03057beaa2162ba4c75f9a3fed282f2b
Raymon0915 <raymon@vasavah.com>
2025-01-31 20:50:56 +0800

    added cards to the trending section

--- surprise-frontend/src/app/components/Trending/Trending.tsx
+++ surprise-frontend/src/app/components/Trending/Trending.tsx
@@ -1,10 +1,16 @@
 export default function Trending() {
   return (
     <section id="trending">
-      <div className="flex flex-col text-start p-4 mt-4">
+      <div className="flex flex-col text-start p-20 pl-60 pr-60 mt-4">
         <h1 className="font-bold text-[24px]">Trending Surprises!</h1>
-        <div className="card">
-            <span>On Sale Now</span>
+        <span>On Sale Now</span>
+        <div className="card mt-7 bg-slate-600 w-full h-80 flex justify-center items-center gap-8">
+          <div className="child-card bg-pink-300 w-48 h-60"></div>
+          <div className="child-card bg-pink-300 w-48 h-60"></div>
+          <div className="child-card bg-pink-300 w-48 h-60"></div>
+          <div className="child-card bg-pink-300 w-48 h-60"></div>
+          <div className="child-card bg-pink-300 w-48 h-60"></div>
+          <div className="child-card bg-pink-300 w-48 h-60"></div>
         </div>
       </div>
     </section>
