88b5b057b55c79ece2ad04c365e94019d27cf5e5
Raymon0915 <raymon@vasavah.com>
2025-03-11 21:46:10 +0800

    fix folder structure

--- surprise-frontend/src/app/layout.tsx
+++ surprise-frontend/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "./components/layout/Header/Header";
+import BottomContent from "./components/layout/BottomContent/BottomContent";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +27,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Sureprise</title>
+      </head>
+      <Header />
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <BottomContent />
     </html>
   );
 }
--- surprise-frontend/src/app/page.tsx
+++ surprise-frontend/src/app/page.tsx
@@ -1,20 +1,16 @@
 import * as React from "react";
-import Header from "./components/layout/Header/Header";
 import Hero from "./components/layout/Hero/Hero";
 import Trending from "./components/layout/Trending/Trending"
 import Categories from "./components/layout/Categories/Categories";
 import JustForYou from "./components/layout/JustForYou/JustForYou";
-import BottomContent from "./components/layout/BottomContent/BottomContent";
 
 export default function Home() {
   return (
     <div className="bg-[#e7e8ec] min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]">
-      <Header />
       <Hero />
       <Trending />
       <Categories />
       <JustForYou />
-      <BottomContent />
     </div>
   );
 }
